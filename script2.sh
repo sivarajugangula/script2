c19f7ed2ad7de4785b3417158eb63bf4feccb7bb
sivarajugangula <128023036+sivarajugangula@users.noreply.github.com>
2023-06-27 11:56:23 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -7,6 +7,4 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
-sudo systemctl start jenkins
-sudo syatemctl enable jenkins
+sudo apt-get install jenkins -y
